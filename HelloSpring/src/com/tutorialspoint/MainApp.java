23f37c3c903202870468d2797325fb0dba71d7f5
vladimir <vvysokomornyi@luxoft.com>
2016-03-30 17:57:10 +0300

    added

---
+++ HelloSpring/src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,22 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+
+		obj.setMessage("hello 2");
+		obj.getMessage();
+		
+		HelloWorld obj2 = (HelloWorld) context.getBean("helloWorld");
+		obj2.getMessage();
+		
+	}
+
+}
